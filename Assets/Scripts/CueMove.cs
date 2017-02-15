6f2d50e325f9d6a84b36bdc5f7c29b26227a5d30
Bohdan Pohonych <bodpoch@gmail.com>
2017-02-15 22:26:21 +0200

    First commit

---
+++ Assets/Scripts/CueMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CueMove : MonoBehaviour {
+    
+    Vector3 dist;
+    float posX;
+    float posY;
+
+    public float moveSpeed;
+
+    void Start()
+    {
+       moveSpeed = 1f;
+    }
+
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0f, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+    }
+
+    void OnMouseDown()
+    {
+        dist = Camera.current.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+    }
+
+
+    void OnMouseDrag()
+    {
+        Vector3 MouseCrntPstn = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y, dist.z);
+        
+        Vector3 ObjPstn = Camera.current.ScreenToWorldPoint(MouseCrntPstn);
+        transform.position = ObjPstn;
+    }
+}
